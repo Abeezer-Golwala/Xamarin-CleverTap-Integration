6956facff3a445e4e544dc57a6ee7f6f534c0d57
abeezer@clevertap.com <qrzvupm19>
2022-03-02 19:35:16 +0530

    2nd commit

---
+++ XamarinCTin.Android/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Com.Clevertap.Android.Sdk;
+using System.Collections.Generic;
+using Java.Util;
+using Firebase.Iid;
+using Android.Gms.Extensions;
+using Android.Util;
+namespace XamarinCTin.Droid
+{
+    [Activity(Label = "XamarinCTin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        CleverTapAPI cleverTapAPI;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+            cleverTapAPI = CleverTapAPI.GetDefaultInstance(Android.App.Application.Context);
+            CleverTapAPI.CreateNotificationChannel(Android.App.Application.Context, "abtest", "abtest", "Xamarin", 5, true);
+            CleverTapAPI.GetDefaultInstance(Android.App.Application.Context).InitializeInbox();
+            PushTokenAsync();//Send token to dashboard
+        }
+
+        private async System.Threading.Tasks.Task PushTokenAsync()
+        {
+            var instanceIdResult = await FirebaseInstanceId.Instance.GetInstanceId().AsAsync<IInstanceIdResult>();
+            string token = instanceIdResult.Token;
+            CleverTapAPI.GetDefaultInstance(Android.App.Application.Context).PushFcmRegistrationId(token, true);
+            Log.Debug("TOken", "token Sent" + token);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        
+    }
+}
---
+++ XamarinCTin/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Android.Util;
+using Com.Clevertap.Android.Sdk;
+using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace XamarinCTin
+{
+    public partial class MainPage : ContentPage
+    {
+        CleverTapAPI cleverTapAPI;
+        public MainPage()
+        {
+            InitializeComponent();
+            cleverTapAPI = CleverTapAPI.GetDefaultInstance(Android.App.Application.Context);
+        }
+        public void createuser(Object sender, EventArgs e)
+        {
+            IDictionary<string, Java.Lang.Object> profileData = new Dictionary<string, Java.Lang.Object>();
+            profileData.Add("Name", "Abeezer Golwala");    // String
+            profileData.Add("Identity", 456);      // String or number
+            profileData.Add("Email", "abeezer@xamarin.com"); // Email address of the user
+            profileData.Add("Phone", "7788994455");   // Phone (with the country code, starting with +)
+            profileData.Add("Gender", "M");             // Can be either M or F
+            profileData.Add("DOB", new Date());         // Date of Birth. Set the Date object to the appropriate value first - requires java.util
+            profileData.Add("MSG-push", true);
+            profileData.Add("MSG-email", true);
+            profileData.Add("MSG-sms", true);
+            profileData.Add("MSG-whatsapp", true);
+            cleverTapAPI.OnUserLogin(profileData);
+        }
+        public void pushnotif(Object sender, EventArgs e)
+        {
+            cleverTapAPI.PushEvent("AbeezerPushEvent");
+            Log.Debug("pushnotif", "pushnotif");
+            
+
+        }
+        public void inapp(Object sender, EventArgs e)
+        {
+            cleverTapAPI.PushEvent("abeezerinapnotif");
+            Log.Debug("inapp", "inapp");
+        }
+        public void getmsg(Object sender, EventArgs e)
+        {
+            cleverTapAPI.PushEvent("Abeezergetmsg");
+            Log.Debug("getmsg", "getmsg");
+        }
+        public void appinbox(Object sender, EventArgs e)
+        {
+            CleverTapAPI.GetDefaultInstance(Android.App.Application.Context).ShowAppInbox();
+            Log.Debug("showinbox", "showinbox");
+        }
+        public void nativedp(Object sender, EventArgs e)
+        {
+            Log.Debug("nativeDp", "debug native");
+        }
+    }
+}
